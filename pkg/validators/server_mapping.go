d4e1c82a0a16ed652407551222b6f5cc7e50f199
Steffen Exler <linuxluigi@users.noreply.github.com>
2025-07-30 10:20:12 +0200

    refactor: Simplify architecture validation logic for ControlPlane and NodePool configurations
    Signed-off-by: Steffen Exler <linuxluigi@users.noreply.github.com>

--- pkg/validators/server_mapping.go
+++ pkg/validators/server_mapping.go
@@ -31,51 +31,71 @@ func GetArchFromServerSize(serverSize string) image.CPUArchitecture {
 // ValidateAndSetArchForControlPlane validates and auto-sets the Arch field for all ControlPlaneNodePoolConfig in ControlPlaneConfig
 // Note: This function works with interface{} to avoid import cycles
 func ValidateAndSetArchForControlPlane(sl validator.StructLevel) {
-	// Get the underlying struct pointer to modify it
 	val := sl.Current()
-	if val.Kind() == reflect.Struct && val.CanSet() {
-		nodePoolsField := val.FieldByName("NodePools")
-		if nodePoolsField.IsValid() && nodePoolsField.CanSet() && nodePoolsField.Kind() == reflect.Slice {
-			for i := 0; i < nodePoolsField.Len(); i++ {
-				nodePoolItem := nodePoolsField.Index(i)
-				if nodePoolItem.Kind() == reflect.Struct && nodePoolItem.CanSet() {
-					// Get the actual values for checking
-					serverSizeField := nodePoolItem.FieldByName("ServerSize")
-					archField := nodePoolItem.FieldByName("Arch")
-
-					if serverSizeField.IsValid() && archField.IsValid() && archField.CanSet() {
-						serverSize := serverSizeField.String()
-						currentArch := archField.String()
-
-						if currentArch == "" {
-							detectedArch := GetArchFromServerSize(serverSize)
-							archField.Set(reflect.ValueOf(detectedArch))
-						}
-					}
-				}
-			}
-		}
+	if !isValidStructForModification(val) {
+		return
 	}
+
+	nodePoolsField := val.FieldByName("NodePools")
+	if !isValidSliceField(nodePoolsField) {
+		return
+	}
+
+	processNodePoolSlice(nodePoolsField)
+}
+
+// isValidStructForModification checks if the value is a valid struct that can be modified
+func isValidStructForModification(val reflect.Value) bool {
+	return val.Kind() == reflect.Struct && val.CanSet()
+}
+
+// isValidSliceField checks if the field is a valid slice that can be modified
+func isValidSliceField(field reflect.Value) bool {
+	return field.IsValid() && field.CanSet() && field.Kind() == reflect.Slice
+}
+
+// processNodePoolSlice processes each node pool in the slice and sets the arch if needed
+func processNodePoolSlice(nodePoolsField reflect.Value) {
+	for i := 0; i < nodePoolsField.Len(); i++ {
+		nodePoolItem := nodePoolsField.Index(i)
+		if !isValidStructForModification(nodePoolItem) {
+			continue
+		}
+
+		setArchIfEmpty(nodePoolItem)
+	}
+}
+
+// setArchIfEmpty sets the Arch field based on ServerSize if it's currently empty
+func setArchIfEmpty(nodePoolItem reflect.Value) {
+	serverSizeField := nodePoolItem.FieldByName("ServerSize")
+	archField := nodePoolItem.FieldByName("Arch")
+
+	if !areFieldsValidForArchUpdate(serverSizeField, archField) {
+		return
+	}
+
+	serverSize := serverSizeField.String()
+	currentArch := archField.String()
+
+	if currentArch == "" {
+		detectedArch := GetArchFromServerSize(serverSize)
+		archField.Set(reflect.ValueOf(detectedArch))
+	}
+}
+
+// areFieldsValidForArchUpdate checks if both serverSize and arch fields are valid for updating
+func areFieldsValidForArchUpdate(serverSizeField, archField reflect.Value) bool {
+	return serverSizeField.IsValid() && archField.IsValid() && archField.CanSet()
 }
 
 // ValidateAndSetArchForNodePool validates and auto-sets the Arch field for NodePoolConfig
 // Note: This function works with interface{} to avoid import cycles
 func ValidateAndSetArchForNodePool(sl validator.StructLevel) {
-	// Use reflection to get the Arch field value
 	val := sl.Current()
-	if val.Kind() == reflect.Struct && val.CanSet() {
-		archField := val.FieldByName("Arch")
-		serverSizeField := val.FieldByName("ServerSize")
-
-		if archField.IsValid() && archField.CanSet() && serverSizeField.IsValid() {
-			currentArch := archField.String()
-			serverSize := serverSizeField.String()
-
-			// If Arch is not set (empty), auto-detect from ServerSize
-			if currentArch == "" {
-				detectedArch := GetArchFromServerSize(serverSize)
-				archField.Set(reflect.ValueOf(detectedArch))
-			}
-		}
+	if !isValidStructForModification(val) {
+		return
 	}
+
+	setArchIfEmpty(val)
 }
