ebb8e30220c951625876d347047a7c89740c2cae
Steffen Exler <linuxluigi@users.noreply.github.com>
2026-03-04 16:14:07 +0100

    fix: comment out node deletion logic in talos-delete-node.sh
    Signed-off-by: Steffen Exler <linuxluigi@users.noreply.github.com>

--- pkg/talos/cli/talos-delete-node.sh
+++ pkg/talos/cli/talos-delete-node.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# Write Talos configuration to a file
-echo $TALOSCONFIG_VALUE > $TALOSCONFIG
+# # Write Talos configuration to a file
+# echo $TALOSCONFIG_VALUE > $TALOSCONFIG
 
-# Reset the node
-if ! talosctl --talosconfig $TALOSCONFIG -n $NODE_IP reset; then
-  echo "ERROR: Talos reset failed for node $NODE_IP" >&2
-  exit 1
-fi
+# # Reset the node
+# if ! talosctl --talosconfig $TALOSCONFIG -n $NODE_IP reset; then
+#   echo "ERROR: Talos reset failed for node $NODE_IP" >&2
+#   exit 1
+# fi
+
+exit 1
